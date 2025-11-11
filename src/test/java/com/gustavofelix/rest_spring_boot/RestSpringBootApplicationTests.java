6764775c2620964aeee6241cba23dc0bea338cde
Gustavo_Félix <gustavofelix2007@gmail.com>
2025-11-11 10:21:34 -0300

    feat: initialize project structure

---
+++ src/test/java/com/gustavofelix/rest_spring_boot/RestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavofelix.rest_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
