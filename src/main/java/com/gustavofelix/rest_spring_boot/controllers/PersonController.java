5e971fe29ce82b1fc926922d67c6fb3b4a724132
Gustavo_Félix <gustavofelix2007@gmail.com>
2025-11-17 08:11:50 -0300

    fix: Versioning all routes

--- src/main/java/com/gustavofelix/rest_spring_boot/controllers/PersonController.java
+++ src/main/java/com/gustavofelix/rest_spring_boot/controllers/PersonController.java
@@ -19,21 +19,21 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/v1", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<PersonDTOV1>> findAll() {
         List<PersonDTOV1> persons = personService.findAll();
 
         return ResponseEntity.ok().body(persons);
     }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/v1/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PersonDTOV1> findById(@PathVariable Long id) {
         PersonDTOV1 person = personService.findById(id);
 
         return ResponseEntity.ok().body(person);
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/v1", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PersonDTOV1> insert(@RequestBody PersonDTOV1 person) {
         PersonDTOV1 createdPersonDTOV1 = personService.insert(person);
 
@@ -59,13 +59,13 @@ public class PersonController {
         return ResponseEntity.created(location).build();
     }
 
-    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(value = "/v1/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PersonDTOV1> update(@PathVariable Long id, @RequestBody PersonDTOV1 person) {
         personService.update(id, person);
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping(value = "/{id}")
+    @DeleteMapping(value = "/v1/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         personService.delete(id);
         return ResponseEntity.noContent().build();
