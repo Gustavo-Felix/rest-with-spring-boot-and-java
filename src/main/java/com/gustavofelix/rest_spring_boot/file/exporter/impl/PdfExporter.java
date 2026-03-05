b560108a5e05d511b057bd7417ee5b27ee2d9519
Gustavo_Félix <gustavofelix2007@gmail.com>
2026-03-05 08:44:12 -0300

    feat: implement PDF export functionality for Person entities using JasperReports

--- src/main/java/com/gustavofelix/rest_spring_boot/file/exporter/impl/PdfExporter.java
+++ src/main/java/com/gustavofelix/rest_spring_boot/file/exporter/impl/PdfExporter.java
@@ -2,15 +2,38 @@ package com.gustavofelix.rest_spring_boot.file.exporter.impl;
 
 import com.gustavofelix.rest_spring_boot.dto.PersonDTO;
 import com.gustavofelix.rest_spring_boot.file.exporter.contract.FileExporter;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class PdfExporter implements FileExporter {
     @Override
     public Resource exportFile(List<PersonDTO> people) throws Exception {
-        return null;
+        InputStream inputStream = getClass().getResourceAsStream("/templates/people.jrxml");
+        if (inputStream == null) {
+            throw new RuntimeException("Template not found: /templates/people.jrxml");
+        }
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(people);
+        Map<String, Object> parameters = new HashMap<>();
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters ,dataSource);
+
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()){
+            JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
+            return new ByteArrayResource(outputStream.toByteArray());
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
