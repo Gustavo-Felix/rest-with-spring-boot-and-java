81febe755c323cb829233218e2562fac094c3c2e
Gustavo_Félix <gustavofelix2007@gmail.com>
2026-03-16 09:43:05 -0300

    feat(exception): add InvalidJWTAuthenticationException and handler for JWT errors

--- src/main/java/com/gustavofelix/rest_spring_boot/exception/handler/CustomEntityResponseHandler.java
+++ src/main/java/com/gustavofelix/rest_spring_boot/exception/handler/CustomEntityResponseHandler.java
@@ -51,4 +51,10 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(InvalidJWTAuthenticationException.class)
+    public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException (Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
