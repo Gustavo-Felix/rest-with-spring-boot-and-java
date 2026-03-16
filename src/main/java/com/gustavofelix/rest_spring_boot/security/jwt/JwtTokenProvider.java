f8708396b2fd058a4758cc05284e0614844a15e6
Gustavo_Félix <gustavofelix2007@gmail.com>
2026-03-16 09:55:26 -0300

    feat(security): implement JwtTokenProvider for JWT token creation and management

---
+++ src/main/java/com/gustavofelix/rest_spring_boot/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,54 @@
+package com.gustavofelix.rest_spring_boot.security.jwt;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import com.gustavofelix.rest_spring_boot.dto.security.TokenDTO;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class JwtTokenProvider {
+
+    @Value("${security.jwt.token.secret-key:secret}")
+    private String secretKey;
+
+    @Value("${security.jwt.token.expire-length:3600000}")
+    private long validityInMilliseconds;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    Algorithm algorithm = null;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        algorithm = Algorithm.HMAC256(secretKey.getBytes());
+    }
+
+    public TokenDTO createAccessToken(String username, List<String> roles) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+        String accessToken = getAccessToken(username, roles, now, validity);
+        String refreshToken = getRefreshToken(username, roles, now);
+
+        return new TokenDTO(username, true, now, validity, accessToken, refreshToken);
+    }
+
+    private String getRefreshToken(String username, List<String> roles, Date now) {
+        return "";
+    }
+
+    private String getAccessToken(String username, List<String> roles, Date now, Date validity) {
+        return "";
+    }
+
+
+}
